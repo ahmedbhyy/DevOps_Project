2e4b67b24d54b9014d06ed1cee72337f536292b5
ahmed <150559634+ahmedbhyy@users.noreply.github.com>
2024-07-03 16:17:08 +0100

    import

--- src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
@@ -2,6 +2,7 @@ package tn.esprit.devops_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 @EntityScan(basePackages = "tn.esprit.devops_project.model")
