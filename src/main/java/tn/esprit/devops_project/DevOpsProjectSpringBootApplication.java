dd89bb74ebbd1612dd6938b0da67d9c6621e525e
ahmed <150559634+ahmedbhyy@users.noreply.github.com>
2024-07-17 16:31:16 +0100

    aezaze

--- src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
+++ src/main/java/tn/esprit/devops_project/DevOpsProjectSpringBootApplication.java
@@ -3,9 +3,11 @@ package tn.esprit.devops_project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan(basePackages = "tn.esprit.devops_project.entities")
+@EnableJpaRepositories(basePackages = "tn.esprit.devops_project.repositories")
 public class DevOpsProjectSpringBootApplication {
 
     public static void main(String[] args) {
